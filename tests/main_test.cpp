3b78064ae49e9aea8433199b722b7c753b8592b7
AlessandroDoraDev <alessandrodoradev@gmail.com>
2025-11-04 21:36:45 +0000

    Added gtest to the project

--- tests/main_test.cpp
+++ tests/main_test.cpp
@@ -1,23 +1,8 @@
-#include <iostream>
-#include <string>
-#include <windows.h>
-#include <ranges>
-#include "globals.h"
-#include "to_strings.h"
-
-void my_assert(bool expression, std::string text_if_failure){
-    if(!expression){
-        std::cout<<text_if_failure<<"\n";
-    }
-}
-
-template<typename ConType, typename ElemType>
-inline bool my_contains(ConType arr, ElemType elem){
-    return std::find(arr.begin(), arr.end(), elem)!=arr.end();
-}
-
-//void test_MultiHashSet();
-
-void main_test(){
-    std::cout<<"Esecuzione test\n";
+#include <gtest/gtest.h>
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
 }
