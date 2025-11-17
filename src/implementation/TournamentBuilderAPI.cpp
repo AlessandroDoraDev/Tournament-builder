19c1b79762ee8f61291f54cc81a38bc367e5e46a
AlessandroDoraDev <alessandrodoradev@gmail.com>
2025-11-17 19:45:07 +0000

    Finished the main GUI and made possible to output the tournament config in html format. Beta 1.0git statusgit statusgit statusgit status! Need to polish the code now..

--- src/implementation/TournamentBuilderAPI.cpp
+++ src/implementation/TournamentBuilderAPI.cpp
@@ -8,25 +8,37 @@
 #include "TournamentConfig.h"
 #include <print>
 
-void buildTournamentFromCSV(std::string players_csv_path, std::string ranks_csv_path) {
+std::map<ErrorMessageCode, std::string> ERROR_MESSAGE_MAP={
+    {COULD_NOT_OPEN_PLAYERS_CSV, "Could not open the players csv file or the file was empty..."},
+    {COULD_NOT_OPEN_RANKS_CSV, "Could not open the ranks csv file or the file was empty..."},
+    {RANKS_FILE_ILL_FORMED,  "Could not load ranks correctly, the ranks csv file was ill-formed..."},
+    {PLAYERS_FILE_ILL_FORMED, "Could not load players correctly, the players csv file was ill-formed..."},
+    {FOUND_PLAYERS_NOT_FITTING_TEAMS_SIZE, "Player list doesn't fit the teams' sizes..."}
+};
 
+BuildTournamentResult buildTournamentFromCSV(std::string players_csv_path, std::string ranks_csv_path) {
     SetConsoleOutputCP(CP_UTF8); //changing windows console decoding to utf8
-    CSVRows players_csv_rows = readCSV(players_csv_path);
-    CSVRows ranks_csv_rows= readCSV(ranks_csv_path);
-    if(!initRankMaps(ranks_csv_rows)){
-        std::println("Could not load ranks...");
-        return;
+    CSVRows rows = readCSV(players_csv_path);
+    if(rows.size()==0){
+        return {TournamentConfig(), COULD_NOT_OPEN_PLAYERS_CSV};
     }
-    PlayerList player_list = formatRowsToPlayerList(players_csv_rows);
+    CSVRows ranks_csv_rows= readCSV(ranks_csv_path);
+    if(ranks_csv_rows.size()==0){
+        return {TournamentConfig(), COULD_NOT_OPEN_RANKS_CSV};
+    }
+    if(!initRankMaps(ranks_csv_rows)){
+        return {TournamentConfig(), RANKS_FILE_ILL_FORMED};
+    }
+    PlayerList player_list = formatRowsToPlayerList(rows);
+    if(player_list.size()==0){
+        return {TournamentConfig(), PLAYERS_FILE_ILL_FORMED};
+    }
+    
     int n_teams = static_cast<int>(player_list.size() / N_PLAYERS);
     Config config=Config(n_teams, N_PLAYERS);
     if(!config.init(player_list)){
-        std::println("Player list doesn't fit the teams' sizes... player list size: {}", 
-            player_list.size());
+        return {TournamentConfig(), FOUND_PLAYERS_NOT_FITTING_TEAMS_SIZE};   
     }
-    timeThis([&](){
-        deduceAndApplyMoves(config);
-    });
-    std::cout<<(std::string)config;
-    std::cout<<(std::string)TournamentConfig(config, player_list);
+    deduceAndApplyMoves(config);
+    return {TournamentConfig(config, player_list), GOOD_TOURNAMENT_RESULT};
 }
--- src/implementation/main_noiaFattaBene.cpp
+++ src/implementation/main_noiaFattaBene.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 #include "TournamentBuilderGui.h"
+#include "TournamentBuilderAPI.h"
 
 
-int main(){
-#ifdef MIO_DEBUG
-    std::cout<<"Siamo in debug mode\n";
-#elif defined(MIO_RELEASE)
-    std::cout<<"Siamo in release mode\n";
-#else
-    std::cout<<"La definizione non ha funzionato\n";
-#endif
-    mainGui();
+int main(int argc, char** argv){
+    if(argc==1){
+        mainGui();
+    }else if(argc==3){
+        BuildTournamentResult res= buildTournamentFromCSV(argv[1], argv[2]);
+        if(res.error_code!=GOOD_TOURNAMENT_RESULT){
+            std::cerr<<ERROR_MESSAGE_MAP.at(res.error_code)<<std::endl;
+            return EXIT_FAILURE;
+        }else{
+            std::cout<<(std::string)res.tournament_config;
+        }
+    }else{
+        std::cerr<<"Bad arguments"<<std::endl;
+    }
+    return EXIT_SUCCESS;
 }
